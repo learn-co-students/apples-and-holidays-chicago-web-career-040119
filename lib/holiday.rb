56efa24accf91a3b01acbf52f8901970a2c8388c
Sonia Dumitru <sonya_dumitru@yahoo.com>
2019-03-04 18:21:05 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,9 +25,11 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-  holiday_hash[:winter].each do |anotimp, data|
-    holiday_hash[:winter][anotimp]<<supply
-  end
+#  holiday_hash[:winter].each do |anotimp, data|
+  #  holiday_hash[:winter][anotimp]<<supply
+#end
+  holiday_hash[:winter][:christmas]<<supply
+  holiday_hash[:winter][:new_years]<<supply
 end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
